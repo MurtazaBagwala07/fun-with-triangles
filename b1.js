49409f24e8edfa852123ca23cef7b2f3ab6ad3c6
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-07-23 01:03:52 +0530

    added functionality to 2 buttons

---
+++ b1.js
@@ -0,0 +1,30 @@
+var angleOne=document.querySelector("#a1");
+var angleTwo=document.querySelector("#a2");
+var angleThree=document.querySelector("#a3");
+
+var outp=document.querySelector("#output");
+
+
+
+function calc(){
+    var angOne=parseInt(angleOne.value);
+    var angTwo=parseInt(angleTwo.value);
+    var angThree=parseInt(angleThree.value);
+
+    console.log(angThree+angTwo+angOne);
+    
+    if(angOne==""||angTwo==NaN||angThree==NaN){
+        alert("Fill Values");
+    } 
+   else{
+       if(angThree+angTwo+angOne==180){
+           outp.innerHTML="POSSIBLE"
+       }
+       else{
+           outp.innerHTML="IMPOSSIBLE(In voice of thanos)"
+       }
+   }
+
+    
+}
+
---
+++ b2.js
@@ -0,0 +1,17 @@
+var angleOne=document.querySelector("#a1");
+var angleTwo=document.querySelector("#a2");
+var angleThree=document.querySelector("#a3");
+
+var outp=document.querySelector("#output");
+
+function calcHypo(){
+    var angOne=parseInt(angleOne.value);
+    var angTwo=parseInt(angleTwo.value);
+    var total=Math.pow(angOne,2)+Math.pow(angTwo,2)
+    
+    total=Math.sqrt(total);
+    // console.log(Math.pow(angOne,2)+Math.pow(angTwo,2))
+    outp.innerHTML="Your Answer is : " + total;
+    // console.log(angTwo);
+    // console.log(angOne);
+}
