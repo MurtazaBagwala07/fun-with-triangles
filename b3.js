697c03af9baed65d9afa8a2a41cdfe60272412a5
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-07-28 21:55:59 +0530

    done

--- b3.js
+++ b3.js
@@ -1,46 +1,60 @@
-var b=document.querySelector("#base");
-var h=document.querySelector("#height");
-var outp=document.querySelector("#output");
+const areaCalBlock = document.querySelectorAll(".areaCalBlock");
+const areaShowHere = document.querySelectorAll(".areaShowHere");
 
-var one = document.querySelector("#a");
-var two = document.querySelector("#b");
-var three = document.querySelector("#c");
+const inputInOption1 = document.querySelectorAll(".inputInOption1");
+const inputInOption2 = document.querySelectorAll(".inputInOption2");
+const inputInOption3 = document.querySelectorAll(".inputInOption3");
 
-var sideOne = document.querySelector("#sA");
-var sideTwo = document.querySelector("#sB");
-var angleOne = document.querySelector("#aA");
+const errorDiv= document.querySelector(".error");
 
-function calculateBH(){
-    var tBase=b.value;
-    var tHeight=h.value;
+const radios = document.forms[0].elements;
+console.log(radios);
 
-    var result =0.5*tBase*tHeight;
-    outp.innerHTML=result;
-    
+radios[0].addEventListener('change', ()=>{
+    showOrHideBlock(areaCalBlock, 0, 1, 2);
+})
+radios[1].addEventListener('change', ()=>{
+    showOrHideBlock(areaCalBlock, 1, 0, 2);
+})
+radios[2].addEventListener('change', ()=>{
+    showOrHideBlock(areaCalBlock, 2, 1, 0);
+})
+
+function showOrHideBlock(blockDivs, showDivIndex, hideDivIndex1, hideDivIndex2){
+    blockDivs[showDivIndex].style.display = "block";
+    blockDivs[hideDivIndex1].style.display = "none";
+    blockDivs[hideDivIndex2].style.display = "none";
 }
 
-function calculateThree(){
-    var sideA = parseInt(one.value);
-    var sideB = parseInt(two.value);
-    var sideC = parseInt(three.value);
 
-    var perimeter = sideA+sideB+sideC;
-    var total = perimeter*(perimeter-sideA)*(perimeter-sideB)*(perimeter-sideC);
-    console.log(total);
-    var ans = Math.sqrt(total);
+document.forms[1].addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const b= Number(inputInOption1[0].value)
+    const h= Number(inputInOption1[1].value)
+    areaShowHere[0].innerText = (b*h)/2
 
-    outp.innerHTML=ans;
-}
+}, false)
 
-function calculateSA(){
-    var i=sideOne.value;
-    var j=sideTwo.value;
-    var k=angleOne.value;
-    console.log(j);
-    var angleToRadian = k * 3.14 /180;
+document.forms[2].addEventListener('submit',(e)=>{
+    e.preventDefault();
+    errorDiv.style.display = "none";
+    const a= Number(inputInOption2[0].value)
+    const b= Number(inputInOption2[1].value)
+    const c= Number(inputInOption2[2].value)
 
-    var result = (i * j * Math.sin(angleToRadian))/2;
-    
-    outp.innerHTML = result;
+    if((a+b)>c && (b+c)>a && (a+c)>b){
+    const s=(a+b+c)/2;
+    areaShowHere[1].innerText= Math.sqrt(s*(s-a)*(s-b)*(s-c));
+    }
+    else{
+        errorDiv.style.display = "block";
+    }  
+}, false )
 
-}
+document.forms[3].addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const b = Number(inputInOption3[0].value)
+    const c = Number(inputInOption3[1].value)
+    const A = Number(inputInOption3[2].value)
+    areaShowHere[2].innerText= (b*c*Math.sin(A*Math.PI/180))/2;  
+}, false )
